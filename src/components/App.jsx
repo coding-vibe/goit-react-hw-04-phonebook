57f14c9ded216bd5caaf215037ec95d0fdda47e1
merry <k.maria.m.24.05@gmail.com>
2023-06-01 03:11:43 +0300

    update useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,12 @@ import Filter from './Filter';
 import { Wrapper } from './App.styled.js';
 
 const App = () => {
-  const [contacts, setContacts] = useState(
-    [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]);
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? JSON.parse(storedContacts) : [];
+  });
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    
-    setContacts(parsedContacts);
-  }, [])
-  
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts])
